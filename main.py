7a4d82a9ba3bd05552687202c0670827fe52a871
Fóti Dávid Bálint (SZF_N_2025_2027) <foti.david.balint@diak.szamalk-szalezi.hu>
2025-12-09 09:23:55 +0100

    map kezdetek

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 # Example file showing a basic pygame "game loop"
 import sys
 import pygame
+import map
 
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
+bg_image = pygame.image.load('background_image.jpg')
 running = True
+map = ["szenbanya","bejarat","kebab","kinai","baguette","angol","amerikai","mexiko","gym","kuria"]
+
 #flip = update screen pygame.display.flip()
 #
 while running:
     for event in pygame.event.get(): # poll for 
-        
+
         if event.type == pygame.QUIT:
             running = False
 
---
+++ map.py
@@ -0,0 +1,5 @@
+#Szénbánya konyhája, bejárati személyzet konyhája, piac standok(kebab, kínai, baguette, angol, amerikai (dupla annyi hízás), taco) 
+#Gym (spammeled az entert úgy fogysz),  kúria (akasztófa)
+def mapgen():
+    pass
+    
